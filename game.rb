5ca29bc99df1a7fd1aeebddd1cc2fba5a8013bf1
Joe Corcoran <joe@corcoran.online>
2017-11-24 12:37:06 +0100

    Background

--- game.rb
+++ game.rb
@@ -4,6 +4,11 @@ class Game < Gosu::Window
   def initialize
     super 1200, 800
     self.caption = 'Fort'
+    @bg = Gosu::Image.new('img/bg.jpg', tileable: true)
+  end
+
+  def draw
+    @bg.draw(0, 0, 0, 1.3, 1.3)
   end
 end
 
